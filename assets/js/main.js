d3b9409401d342b9a61e8a04c01cd818f35a5906
Erik Stewart-Smith <59583004+erik1968@users.noreply.github.com>
2023-01-27 08:39:53 -0800

    Added All Pixelarity Scalar Theme Files Folders
    Copied all the Pixelarity Scalar theme html, css, contact, etc, folders to this new Repo for a Gilman Family Practice website [replaces the current customized Stackbit Jamstack that is deprecated and no longer works w/Netlify Build Deploys due to old code/configs no longer work]

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+/*
+	Scalar by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$html = $('html');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'center',
+			hideDelay: 350
+		});
+
+	// Nav Panel.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'top',
+					target: $html,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
